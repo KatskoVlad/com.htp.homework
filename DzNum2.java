206aa11bfc5977a1f17744f9d4f43be01d2a5cae
KatskoVlad <44637988+KatskoVlad@users.noreply.github.com>
2018-11-14 23:30:46 +0300

    Add files via upload

--- DzNum2.java
+++ DzNum2.java
@@ -1,8 +1,5 @@
-package homework.HomeWork2;
+package homework.homeWork2;
 
-import com.sun.org.apache.xpath.internal.operations.Equals;
-
-import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Random;
 import java.util.Scanner;
@@ -21,8 +18,8 @@ public class DzNum2 {
 
         //string array
         Scanner scanner = new Scanner(System.in);
-        String [] str = new String[10];
-        for (int i = 0; i <10 ; i++) {
+        String[] str = new String[10];
+        for (int i = 0; i < 10; i++) {
             str[i] = scanner.nextLine();
         }
 
@@ -75,12 +72,12 @@ public class DzNum2 {
         repeatIntegerElements(arr);
 
         System.out.println("Task 13");
-        String [] arrString = {"Ivan", "Alex", "Slava", "Dima", "Ignat", "Vlad", "Oleg", "Maxim", "Ivan", "Pavel"};
+        String[] arrString = {"Ivan", "Alex", "Slava", "Dima", "Ignat", "Vlad", "Oleg", "Maxim", "Ivan", "Pavel"};
         repeatStringElements(arrString);
 
         System.out.println("Task 14");
-        String [] arrString1 = {"Ivan", "Alex", "Slava", "Dima", "Ignat", "Vlad", "Oleg", "Maxim", "Ivan", "Pavel"};
-        String [] arrString2 = {"Ivan1", "Alex", "Slava", "Dima2", "Ignat3", "Vlad", "Oleg4", "Maxim5", "Ivan6", "Pavel"};
+        String[] arrString1 = {"Ivan", "Alex", "Slava", "Dima", "Ignat", "Vlad", "Oleg", "Maxim", "Ivan", "Pavel"};
+        String[] arrString2 = {"Ivan1", "Alex", "Slava", "Dima2", "Ignat3", "Vlad", "Oleg4", "Maxim5", "Ivan6", "Pavel"};
         repeatStringTwoElements(arrString1, arrString2);
 
         System.out.println("Task 15");
@@ -117,6 +114,7 @@ public class DzNum2 {
             System.out.println(element);
         }
     }
+
     // Task 1
     private static int[] sort(int[] arr) {
         int index = 0;
@@ -139,6 +137,7 @@ public class DzNum2 {
         }
         return arr;
     }
+
     // Task 2
     private static int sum(int[] arr) {
         int summa = 0;
@@ -151,6 +150,7 @@ public class DzNum2 {
     private static void printInt(int summa) {
         System.out.println(summa);
     }
+
     // Task 3
     private static void paintGrid(String symbol, int[][] p) {
         for (int i = 0; i < 5; i++) {
@@ -160,21 +160,23 @@ public class DzNum2 {
             System.out.println("");
         }
     }
+
     // Task 5
     private static void findElement(int element, int[] arr) {
-        boolean foundElement=false;
+        boolean foundElement = false;
         for (int i = 0; i < arr.length; i++) {
             if (arr[i] == element) {
                 foundElement = true;
                 break;
             }
         }
-        if (foundElement){
+        if (foundElement) {
             System.out.println("Set element exists in this array!");
         } else {
             System.out.println("Set element does not exist in this array!");
         }
     }
+
     // Task 6
     private static int findIndex(int element, int[] arr) {
         int index = 0;
@@ -185,26 +187,29 @@ public class DzNum2 {
         }
         return index;
     }
+
     // Task 7
     private static int[] removeElement(int element, int[] arr) {
-        int[] array = new int[arr.length-1];
+        int[] array = new int[arr.length - 1];
         int indexRemove = 0;
         for (int i = 0; i < arr.length; i++) {
             if (arr[i] == element) {
                 indexRemove = i;
             }
-            if (indexRemove==0){
+            if (indexRemove == 0) {
                 array[i] = arr[i];
-            }else {
-                array[indexRemove]=arr[i+1];
+            } else {
+                array[indexRemove] = arr[i + 1];
                 indexRemove++;
             }
-            if (arr.length-1==indexRemove)
+            if (arr.length - 1 == indexRemove) {
                 break;
+            }
         }
 
         return array;
     }
+
     // Task 8
     private static int[] copyArray(int[] arr) {
         int[] array = new int[arr.length];
@@ -213,106 +218,124 @@ public class DzNum2 {
         }
         return array;
     }
+
     // Task 9
-    private static int[] insertArrayOnIndex(int element, int index, int[] arr){
+    private static int[] insertArrayOnIndex(int element, int index, int[] arr) {
         arr[index] = element;
         return arr;
     }
+
     // Task 10
-    private static int maxElement(int[] arr){
+    private static int maxElement(int[] arr) {
         int max = arr[0];
-        for (int i = 0; i <arr.length; i++) {
-            if (arr[i]>max)
+        for (int i = 0; i < arr.length; i++) {
+            if (arr[i] > max) {
                 max = arr[i];
+            }
         }
         return max;
     }
-    private static int minElement(int[] arr){
+
+    private static int minElement(int[] arr) {
         int min = arr[0];
-        for (int i = 0; i <arr.length; i++) {
-            if (arr[i]<min)
+        for (int i = 0; i < arr.length; i++) {
+            if (arr[i] < min) {
                 min = arr[i];
+            }
         }
         return min;
     }
+
     // Task 11
-    private static int[] changeArray(int[] arr){
-        int [] array = new int[arr.length];
+    private static int[] changeArray(int[] arr) {
+        int[] array = new int[arr.length];
         int i = 0;
-        for (int j = arr.length-1; j >=0; j--) {
+        for (int j = arr.length - 1; j >= 0; j--) {
             array[i] = arr[j];
             i++;
         }
         return array;
     }
+
     // Task 12
-    private static void repeatIntegerElements(int [] arr){
+    private static void repeatIntegerElements(int[] arr) {
         for (int i = 0; i < arr.length; i++) {
-            for (int j = i+1; j < arr.length; j++) {
-                if (arr[j]==arr[i]) {
+            for (int j = i + 1; j < arr.length; j++) {
+                if (arr[j] == arr[i]) {
                     System.out.println("Repeating integer element in Array: " + arr[i]);
                     break;
                 }
             }
         }
     }
+
     // Task 13
     private static void repeatStringElements(String[] arrStr) {
         for (int i = 0; i < arr.length; i++) {
-            for (int j = i+1; j < arrStr.length; j++) {
-                if ((arrStr[j]).equals(arrStr[i]))
-                    System.out.println("Repeating string in Array: "+arrStr[i]);
+            for (int j = i + 1; j < arrStr.length; j++) {
+                if ((arrStr[j]).equals(arrStr[i])) {
+                    System.out.println("Repeating string in Array: " + arrStr[i]);
+                }
             }
         }
     }
+
     // Task 14
     private static void repeatStringTwoElements(String[] arr1, String[] arr2) {
         for (int i = 0; i < arr1.length; i++) {
             for (int j = 0; j < arr2.length; j++) {
-                if (arr1[i].equals(arr2[j]))
+                if (arr1[i].equals(arr2[j])) {
                     System.out.println("Repeating to String in two Array: " + arr1[i]);
+                }
             }
         }
     }
+
     // Task 15
     private static void repeatIntegerTwoElements(int[] arr1, int[] arr2) {
         for (int i = 0; i < arr1.length; i++) {
             for (int j = 0; j < arr2.length; j++) {
-                if (arr1[i] == arr2[j])
+                if (arr1[i] == arr2[j]) {
                     System.out.println("Repeating to String in two Array: " + arr1[i]);
+                }
             }
         }
     }
+
     // Task 15
     private static int[] removeRepeatInteger(int[] arr) {
         int unique = 1;
-        for (int i = 1; i <arr.length ; i++) {
-            if (arr[i] != arr[i-1])
+        for (int i = 1; i < arr.length; i++) {
+            if (arr[i] != arr[i - 1]) {
                 unique++;
+            }
         }
         int[] resultat = new int[unique];
         int k = 0;
-        if (resultat.length>0)
+        if (resultat.length > 0)
             resultat[k++] = arr[0];
-        for (int i = 1; i < arr.length ; i++) {
-            if (arr[i] != arr[i-1])
+        for (int i = 1; i < arr.length; i++) {
+            if (arr[i] != arr[i - 1]) {
                 resultat[k++] = arr[i];
+            }
         }
         return resultat;
     }
+
     // Task 16
-    private static int sortReverce(int[] arr){
+    private static int sortReverce(int[] arr) {
         int[] array = new int[arr.length];
         arr = sort(arr);
         int index = 0;
-        for (int i = arr.length-1; i >0 ; i--) {
+        for (int i = arr.length - 1; i > 0; i--) {
             array[index] = arr[i];
             index++;
         }
         return array[1];
     }
+
     // Task 17
-    private static int retriveTwoMaxElem(int [] array){
+    private static int retriveTwoMaxElem(int[] array) {
 //        int high1 = Integer.MAX_VALUE;
 //        int high2 = Integer.MAX_VALUE;
 //        for (int i : array) {
@@ -338,44 +361,46 @@ public class DzNum2 {
 //        }
 //        return max;
         Arrays.sort(array);
-        int index = array.length-1;
-        while(array[index]==array[array.length-1]){
+        int index = array.length - 1;
+        while (array[index] == array[array.length - 1]) {
             index--;
         }
-    return array[index];
+        return array[index];
     }
+
     // Task 18
-    private static int retriveTwoMinElem(int[] array){
+    private static int retriveTwoMinElem(int[] array) {
         int high1 = Integer.MIN_VALUE;
         int high2 = Integer.MIN_VALUE;
-        for (int i : array  ) {
-            if (i>high1) {
+        for (int i : array) {
+            if (i > high1) {
                 high2 = high1;
                 high1 = i;
-            } else if (i>high2) {
+            } else if (i > high2) {
                 high2 = i;
             }
         }
         return high2;
     }
+
     // Task 19
-    private static void createTwoMatrixOneSize(){
+    private static void createTwoMatrixOneSize() {
         Random random = new Random();
-        int [][] matrica1 = new int [2][2];
-        int [][] matrica2 = new int [2][2];
-        for (int i = 0; i <matrica1.length ; i++) {
-            for (int j = 0; j <matrica1.length ; j++) {
+        int[][] matrica1 = new int[2][2];
+        int[][] matrica2 = new int[2][2];
+        for (int i = 0; i < matrica1.length; i++) {
+            for (int j = 0; j < matrica1.length; j++) {
                 matrica1[i][j] = random.nextInt();
             }
         }
-        for (int i = 0; i <matrica2.length ; i++) {
-            for (int j = 0; j <matrica2.length ; j++) {
+        for (int i = 0; i < matrica2.length; i++) {
+            for (int j = 0; j < matrica2.length; j++) {
                 matrica2[i][j] = random.nextInt();
             }
         }
-        for (int i = 0; i <matrica1.length ; i++) {
-            for (int j = 0; j <matrica2.length ; j++) {
-                System.out.println(matrica1[i][j]+" + "+matrica2[i][j]+" = "+(matrica1[i][j]+matrica2[i][j]));
+        for (int i = 0; i < matrica1.length; i++) {
+            for (int j = 0; j < matrica2.length; j++) {
+                System.out.println(matrica1[i][j] + " + " + matrica2[i][j] + " = " + (matrica1[i][j] + matrica2[i][j]));
             }
         }
     }
